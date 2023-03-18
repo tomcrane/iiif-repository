39bdee679ab2b6729a9341eb92529c81f60d0ad0
tomcrane <tom@nanoplanet.com>
2023-03-18 18:39:25 +0000

    start checking payload

--- IIIFRespository/Requests/IIIFBody.cs
+++ IIIFRespository/Requests/IIIFBody.cs
@@ -10,13 +10,43 @@ public class IIIFBody
         {
             using var doc = JsonDocument.Parse(body);
             Type = doc.RootElement.GetProperty("type").GetString();
-            Id = doc.RootElement.GetProperty("id").GetString();
+            JsonElement idElement;
+            if(doc.RootElement.TryGetProperty("id", out idElement))
+            {
+                Id = idElement.GetString();
+                if (!string.IsNullOrWhiteSpace(Id))
+                {
+                    var uri = new Uri(Id);
+                    // does the path match the id?
+                    var uriParts = uri.PathAndQuery.Split('?');
+                    if (uriParts.Length > 1)
+                    {
+                        ProcessingError = "Resource id includes a query string";
+                    }
+                    else
+                    {
+                        var uriPathElements = uriParts[0].Split('/');
+                        IdPathElement = uriPathElements.Last();
+
+                        // if the last element differs it might indicate that we are renaming
+                        // if the id is missing we're asking to mint one (?)
+                        // these things depend on what the thing is and what the operation is, we juse need to assemble data for the controller to decide.
+
+                        //   /iiif/some/path/
+                        //   /iiif/some/path/manifest
+                        //   /iiif/some/path
+                        //   /iiif/some/path/manifest/  xx
+
+                        // see if the start elements are the same
+
+                        // see if resource already exists
+
+                    }
+                }
+            }
 
             var pathElements = path.Split('/');
-            var IdElements = Id.Split('/');
-
             TargetPathElement = pathElements.Last();
-            IdPathElement = IdElements.Last();
 
             JsonElement items;
             int itemCount = -1;
